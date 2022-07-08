b8e072cba8c747fa46007797a38b196a070cce4e
Adhinarayanan perumal <uadhinarayanan@gmail.com>
2022-07-08 12:02:37 +0530

    delete

--- VIEWCODE/src/Dashboard.js
+++ VIEWCODE/src/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
 
 
 export default function Dashboard() {
@@ -9,21 +10,37 @@ export default function Dashboard() {
   const [loading, setLoading] = useState(true)
 
 
+  
+
+
   useEffect(() => {
     setLoading(true);
-
-    async function listEmployees() {
-      const response = await fetch('http://localhost:8080/employee/listEmp');
-      const json = await response.json();
-      setEmployee(json);
-      console.log(json);
-      loaded = true;
-      setLoading(false);
-
-    }
-    listEmployees();
+    listEmp();
     console.log("Call fired....");
   }, []);
+
+
+  const remove = async (empId) => {
+    await fetch(`http://localhost:8080/employee/deleteEmp/${empId}`, {
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).then(() => {
+      listEmp();
+    });
+  }
+
+  async function listEmp() {
+    const response = await fetch('http://localhost:8080/employee/listEmp');
+    const json = await response.json();
+    setEmployee(json);
+    console.log(json);
+    loaded = true;
+    setLoading(false);
+  }
+
 
   if (loading) {
     return <p>Loading...</p>;
@@ -31,6 +48,12 @@ export default function Dashboard() {
 
   return (
     <div>
+      <ul>
+        <li>
+          <Link to="/registerNewEmployee">Go to Register Employee Screen</Link>
+        </li>
+      </ul>
+
       <h2>Our dashboard details</h2>
       <table border="2">
         <thead>
@@ -43,14 +66,13 @@ export default function Dashboard() {
         </thead>
         <tbody>
           {
-
-
             employee.listEmp.map(e => (
               <tr>
                 <td> {e.empId}</td>
                 <td >{e.employeeName}</td>
                 <td >{e.age}</td>
                 <td >{e.status}</td>
+                <a onClick={() => remove(e.empId)}>Delete</a>
               </tr>
             ))
 
